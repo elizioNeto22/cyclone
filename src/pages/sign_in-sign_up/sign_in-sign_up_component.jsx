2e2e46c6593dca11aa76a1d996e6c1635ac34759
Elizio Neto <epe_elizio@hotmail.com>
2020-01-29 22:43:15 -0300

    added sign up  and firestore database

--- src/pages/sign_in-sign_up/sign_in-sign_up_component.jsx
+++ src/pages/sign_in-sign_up/sign_in-sign_up_component.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import SignIn from '../../components/sign_in/sign_in_component'
+import SignUp from '../../components/sign_up/sign_up_component'
 
 import './sign_in-sign_up_styles.scss'
 
 const SignInAndSignUpPage = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 )
 

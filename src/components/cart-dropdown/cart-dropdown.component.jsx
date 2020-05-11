177e6b069a76ac58c9754cfdd7d40b7afedc7679
elizioNeto22 <elizioNeto@protonmail.com>
2020-05-10 23:06:44 -0300

    hide the dropdown when entering the checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,12 +6,14 @@ import { withRouter } from 'react-router-dom'
 import CartItem from '../cart-item/cart-item.component'
 import CustomButton from '../custom_button/custom_button_component'
 import { selectCartItems } from '../../redux/cart/cart.selectors'
+import { toggleCart } from '../../redux/cart/cart.actions'
 
 import './cart-dropdown.styles.scss'
 
-const CartDropdown = ({ cartItems, history }) => {
+const CartDropdown = ({ cartItems, history, dispatch }) => {
   const renderDropdownItem = () =>
     cartItems.map((item) => <CartItem key={item.id} item={item} />)
+
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
@@ -21,7 +23,12 @@ const CartDropdown = ({ cartItems, history }) => {
           <span className="empty-message">Your cart is empty</span>
         )}
       </div>
-      <CustomButton onClick={() => history.push('/checkout')}>
+      <CustomButton
+        onClick={() => {
+          history.push('/checkout')
+          dispatch(toggleCart())
+        }}
+      >
         GO TO CHECKOUT
       </CustomButton>
     </div>
@@ -35,6 +42,10 @@ const mapStateToProps = createStructuredSelector({
 export default withRouter(connect(mapStateToProps)(CartDropdown))
 
 /* 
+  connect passes DISPATCH into our component as a prop
+  if we do not suply a second argument to connect.
+  The reason is because if we need to make ONE action dispatch will just be more verbose 
+
   about falsy elements if:
   • it's an OBJECT we set them by default to NULL
   • it's an NUMBER we set it to zero 0

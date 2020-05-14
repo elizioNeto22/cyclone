294b1e9c13d8b23939cf7144076c279f2dcbec9b
elizioNeto22 <elizioNeto@protonmail.com>
2020-05-13 23:52:14 -0300

    recovery

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,14 +1,14 @@
-import {createStore, applyMiddleware} from 'redux'
-import {logger} from 'redux-logger'
-import {composeWithDevTools} from 'redux-devtools-extension'
+import { createStore, applyMiddleware } from 'redux'
+import { logger } from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 import rootReducer from '../root-reducer'
 
 const middlewares = [logger]
 
-const store = createStore(rootReducer, composeWithDevTools(
-  applyMiddleware(...middlewares),
-));
-
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middlewares))
+)
 
 export default store

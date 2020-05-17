8448c2e0e27e7a5d94991f9dd5beda3960bade57
elizioNeto22 <elizioNeto@protonmail.com>
2020-05-17 19:46:04 -0300

    wrap the App with PersistGate component and import persistor from store

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter} from 'react-router-dom'
-import {Provider} from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
-import store from './redux/store/store'
+import { persistor, store } from './redux/store/store'
 import App from './App'
 
 import './index.css'
 
-
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
-    document.getElementById('root')
+  document.getElementById('root')
 )
-
